e7eb88899078d3f34d87086509733dbeaa2cfd05
김진우 <jinwoo07.kim@lgepartner.com>
2023-03-02 14:13:08 +0900

    feature : TypeORM / pg / reflect-metadata install

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,22 @@
-import express from 'express';
-import morgan from 'morgan';
+import express from "express";
+import morgan from "morgan";
+import { AppDataSource } from "./data-source";
 
 const app = express();
 
 // json요청을 해석
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/', (_, res) => res.send('running'));
+app.get("/", (_, res) => res.send("running"));
 
 let port = 4000;
 app.listen(port, async () => {
-	console.log(`server running at http://localhost:${port}`);
+  console.log(`server running at http://localhost:${port}`);
+
+  AppDataSource.initialize()
+    .then(async () => {
+      console.log("Inserting a new user into the database...");
+    })
+    .catch((error) => console.log(error));
 });
