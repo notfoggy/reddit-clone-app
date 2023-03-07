d5f21b91250c949fabec981f301e319292634249
김진우 <jinwoo07.kim@lgepartner.com>
2023-03-07 16:54:31 +0900

    [FRONTEND] register페이지 작성

---
+++ client/src/components/inputGroup.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import classNames from "classnames";
+
+interface InputGroupPorps {
+  className?: string;
+  type?: string;
+  placeholder?: string;
+  value?: string;
+  error: string | undefined;
+  setValue: (str: string) => void;
+}
+
+const InputGroup: React.FC<InputGroupPorps> = ({
+  className = "mb-2",
+  type = "text",
+  placeholder = "",
+  value = "",
+  error = "",
+  setValue,
+}) => {
+  return (
+    <div className={className}>
+      <input
+        type={type}
+        style={{ minWidth: 300 }}
+        className={classNames(
+          "w-full p-3 transition duration-200 border border-gray-400 rounded bg-gray-50 focus:bg-white hover:bg-white",
+          { "border-red-500": error }
+        )}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <small className="font-medium text-red-500">{error}</small>
+    </div>
+  );
+};
+
+export default InputGroup;
