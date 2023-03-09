f8135def2a50d16da01483d9c71df617d4f51b1d
jinwoo <notfoggy2@gmail.com>
2023-03-09 23:12:16 +0900

    [FRONTEND][fix] register후 login페이지로 이동하도록 설정

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,70 +1,70 @@
-import axios from "axios";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import React, { FormEvent, useState } from "react";
-import InputGroup from "../components/inputGroup";
+import axios from 'axios';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { FormEvent, useState } from 'react';
+import InputGroup from '../components/inputGroup';
 
 const Register = () => {
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErros] = useState<any>({});
+	const [email, setEmail] = useState('');
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [errors, setErros] = useState<any>({});
 
-  const router = useRouter();
+	const router = useRouter();
 
-  const handleSubmit = async (event: FormEvent) => {
-    event.preventDefault();
-    try {
-      const res = await axios.post("/auth/register", {
-        email,
-        username,
-        password,
-      });
-      console.log("res>>", res);
-      // router.push("/login");
-    } catch (error: any) {
-      console.log("error", error);
-      setErros(error.response.data || {});
-    }
-  };
-  return (
-    <div className="bg-white">
-      <div className="flex flex-col items-center justify-center h-screen p-6">
-        <div className="w-10/12 mx-auto md:w-96">
-          <h1 className="mb-2 text-lg font-medium">회원가입</h1>
-          <form onSubmit={handleSubmit}>
-            <InputGroup
-              placeholder="Email"
-              value={email}
-              setValue={setEmail}
-              error={errors?.email}
-            />
-            <InputGroup
-              placeholder="Username"
-              value={username}
-              setValue={setUsername}
-              error={errors?.username}
-            />
-            <InputGroup
-              placeholder="Password"
-              value={password}
-              setValue={setPassword}
-              error={errors?.password}
-            />
-            <button className="w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded">
-              회원 가입
-            </button>
-          </form>
-          <small>
-            이미 가입하셨나요?
-            <Link href="/login" className="ml-1 text-blue-500 uppercase">
-              로그인
-            </Link>
-          </small>
-        </div>
-      </div>
-    </div>
-  );
+	const handleSubmit = async (event: FormEvent) => {
+		event.preventDefault();
+		try {
+			const res = await axios.post('/auth/register', {
+				email,
+				username,
+				password,
+			});
+			console.log('res>>', res);
+			router.push('/login');
+		} catch (error: any) {
+			console.log('error', error);
+			setErros(error.response.data || {});
+		}
+	};
+	return (
+		<div className="bg-white">
+			<div className="flex flex-col items-center justify-center h-screen p-6">
+				<div className="w-10/12 mx-auto md:w-96">
+					<h1 className="mb-2 text-lg font-medium">회원가입</h1>
+					<form onSubmit={handleSubmit}>
+						<InputGroup
+							placeholder="Email"
+							value={email}
+							setValue={setEmail}
+							error={errors?.email}
+						/>
+						<InputGroup
+							placeholder="Username"
+							value={username}
+							setValue={setUsername}
+							error={errors?.username}
+						/>
+						<InputGroup
+							placeholder="Password"
+							value={password}
+							setValue={setPassword}
+							error={errors?.password}
+						/>
+						<button className="w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded">
+							회원 가입
+						</button>
+					</form>
+					<small>
+						이미 가입하셨나요?
+						<Link href="/login" className="ml-1 text-blue-500 uppercase">
+							로그인
+						</Link>
+					</small>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Register;
